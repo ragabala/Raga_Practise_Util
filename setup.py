f1fa6cd1738ce29c284dc5d6f7b927f44ca86e8e
ragabala <ragavendranb92@gmail.com>
2018-06-06 11:26:37 -0400

    modifying version

--- setup.py
+++ setup.py
@@ -8,10 +8,11 @@ requirements=['packaging']
 
 setup(
         name='raga_practice_util',
-        description='This is a sample project',
+        description='This is a quick start helper project for helping beginners create a python unit test package distribution',
         long_description = long_description,
         url = 'https://github.com/ragabala/Raga_Practise_Util',
         author = 'Ragavendran balakrishnan',
+        version = '0.0.1',
         author_email = 'ragbalak@redhat.com',
         license='TEST',
         install_requires = requirements,
